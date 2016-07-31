12b0a821b364cd61a3fba3363553402a096cbd77
chirath <chirath.02@gmail.com>
2016-07-31 14:55:23 +0530

    finished signup

--- mobile/views.py
+++ mobile/views.py
@@ -29,3 +29,24 @@ class SignupView(View):
     def get(self, request):
         form = self.form_class(None)
         return render(request, self.template_name, {'form':form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+            user = form.save(commit=False)
+
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user.set_password(password)
+            user.save()
+
+            user = authenticate(username=username, password=password)
+
+            if user.is_active:
+                login(request, user)
+                return redirect('home')
+
+        return render(request,  self.template_name, {'form':form})
+
