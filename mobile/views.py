e2237e10c868337b36a72c1c9b4d4c93e2943064
chirath <chirath.02@gmail.com>
2016-07-31 14:00:58 +0530

    Fixed a bug in forms:

--- mobile/views.py
+++ mobile/views.py
@@ -23,7 +23,7 @@ class AddMobileView(CreateView):
 
 class SignupView(View):
     form_class = UserForm
-    template_name = 'SignUp.html'
+    template_name = 'signUp.html'
 
 
     def get(self, request):
